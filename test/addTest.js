e91a64d7a54826aa977407998d7831c115820638
Jan Dvorak <hohnzy@gmail.com>
2017-02-12 17:27:35 +0100

    Removing fat arrow functions

--- test/addTest.js
+++ test/addTest.js
@@ -2,12 +2,12 @@ var assert = require('chai').assert;
 var add = require('../add');
 
 describe('Add', () => {
-    it('should add 5 and return 10', () => {
+    it('should add 5 and return 10', function() {
         var result = add(5);
         assert.equal(result, 10);
     });
 
-    it('should return type number', () => {
+    it('should return type number', function() {
         var result = add(5);
         assert.typeOf(result, 'number');
     });
